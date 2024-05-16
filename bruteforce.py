ca3d838e908a568c7b6dbc2661b91b56d5fe8f90
Steve <mas.ste@gmail.com>
2024-05-16 20:37:51 +0200

    First version of program

---
+++ bruteforce.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import random
+
+FILE_PATH = 'datas/datas.xlsx'
+MONEY = 500
+
+
+def choose_actions_to_buy():
+    stock_actions_list = []
+    boucle = 30000
+    while boucle > 0:
+
+        datas = pd.read_excel(FILE_PATH)
+        actions_list = datas.to_dict(orient='records')
+
+        nb_actions = random.randint(1, 17)
+        choosen_actions = []
+
+        # create stock portfolio with random actions
+        for i in range(nb_actions):
+            rand_action = random.choice(actions_list)
+            choosen_actions.append(rand_action)
+            actions_list.remove(rand_action)
+
+        # sort actions lists, check if choosen actions not already in list and if cost < money account
+        sorted_actions_names = sorted(choosen_actions, key=lambda dico: int(dico['name'].strip('#')))
+
+        if sorted_actions_names not in stock_actions_list:
+            if sum([action["value"] for action in sorted_actions_names]) < MONEY:
+                stock_actions_list.append(choosen_actions)
+                boucle -= 1
+                print(f"Non tested : {boucle}")
+            else:
+                boucle -= 1
+                print(f"Non tested : {boucle}")
+
+    for actions in stock_actions_list:
+
+        total_gain = 0
+        gain_action = 0
+        for action in actions:
+            gain_action += action["value"] * action["gain"]
+            action["gain action"] = gain_action
+            total_gain += gain_action
+
+        actions.append(total_gain)
+
+    best = max(stock_actions_list, key=lambda x: x[-1])
+
+    print(f"Total best gain : {round(best[-1], 2)}")
+    best.pop(-1)
+    best_names = [action["name"] for action in best]
+    print(f"Winners : {best_names}")
+    print(f"Start invest : {sum([action["value"] for action in best])}")
+
+
+choose_actions_to_buy()
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import pandas as pd
-import random
+import bruteforce
 
-file_path = 'datas/datas.xlsx'
-df = pd.read_excel(file_path)
-dico = df.to_dict(orient='records')
-
-wallet = 500
-new_dico = []
-while wallet > 0:
-    rand_action = random.choice(dico)
-    if wallet < rand_action["cout"]:
-        break
-    wallet -= rand_action["cout"]
-    dico.remove(rand_action)
-    new_dico.append(rand_action)
-
-print(f"reste wallet : {wallet}$.")
-total_benef = 0
-for action in new_dico:
-    benef = action["cout"] * action["benef"]
-    total_benef += benef
-
-print(f"total benef 2 ans : {total_benef:.2f}$.")
+bruteforce.choose_actions_to_buy()
