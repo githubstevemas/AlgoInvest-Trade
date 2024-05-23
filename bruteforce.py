8a95253db21852ecf74fbf19d54b1e569e1e6f25
Steve <mas.ste@gmail.com>
2024-05-23 12:39:21 +0200

    Refactoring code for brute and opti versions, adding timer

--- bruteforce.py
+++ bruteforce.py
@@ -1,61 +1,63 @@
 import pandas as pd
 import itertools
+import time
 
 FILE_PATH = 'datas/datas00.xlsx'
 MONEY = 500
 
 
-def calculate_gain(combinations_list):
-
-    for actions in combinations_list:
-
-        total_gain = 0
-        gain_action = 0
-        for action in actions:
-            gain_action += action[1] * action[2]
-            action.append(gain_action)
-            total_gain += gain_action
-        actions.append(total_gain)
-
-    return combinations_list
-
-
 def pick_actions():
-
     combinations_list = []
     datas = pd.read_excel(FILE_PATH, header=None)
     actions_list = datas.values.tolist()
 
     for i in range(1, len(actions_list) + 1):
         combinations = list(itertools.combinations(actions_list, i))
-
         for actions in combinations:
-            print(actions)
-
             if sum(action[1] for action in actions) <= MONEY:
-                print("prix combo ok")
-                combinations_list.append(list(actions))
-            else:
-                print("prix trop cher pour notre petit porte monaie..")
+                combinations_list.append(actions)
 
     return combinations_list
 
 
-def display_winning_combo(final_list):
-    best = max(final_list, key=lambda x: x[-1])
-
-    print(f"Total best gain : {round(best[-1], 2)}")
-    best.pop(-1)
-    best_names = [action[0] for action in best]
-    print(f"Winners : {best_names}")
-    print(f"Start invest : {sum([action[1] for action in best])}")
+def calculate_gain(actions):
+    total_gain = 0
+    for action in actions:
+        gain = action[1] * (1 + action[2])
+        total_gain += gain
+    return total_gain
 
 
-def run_brute():
+def check_best_combination():
     combinations_list = pick_actions()
-    final_list = calculate_gain(combinations_list)
-    display_winning_combo(final_list)
+    best_combination = None
+    best_combinaison_gain = 0
+
+    for combinaison in combinations_list:
+        combinaison_gain = calculate_gain(combinaison)
+        if combinaison_gain > best_combinaison_gain:
+            best_combinaison_gain = combinaison_gain
+            best_combination = combinaison
+
+    return best_combination, best_combinaison_gain
+
+
+def run_bruteforce():
+
+    start_time = time.time()
+
+    best_combination, best_combinaison_gain = check_best_combination()
+
+    best_actions_names = [action[0] for action in best_combination]
+    total_invest = sum([action[1] for action in best_combination])
+
+    print(f"Start invest : {total_invest}")
+    print(f"Total value after 2y : {round(best_combinaison_gain, 2)}")
+    print(f"Recommended actions : {best_actions_names}")
+
+    total_time = time.time() - start_time
+    print(round(total_time, 2))
 
 
 if __name__ == "__main__":
-    run_brute()
+    run_bruteforce()
--- optimized.py
+++ optimized.py
@@ -1,34 +1,61 @@
 import pandas as pd
+import time
 
-FILE_PATH = 'datas/datas01.xlsx'
+FILE_PATH = 'datas/datas00.xlsx'
 MONEY = 500
 
 
-def run_optimized(gains, values, max_money):
+def pick_actions(gains, values, names):
 
-    n = len(actions_names)
+    combinaisons = []
+    for i in range(len(names) + 1):
+        combinaisons.append([])
+        for j in range(MONEY + 1):
+            combinaisons[i].append(0)
 
-    dp = []
-    for i in range(n + 1):
-        dp.append([])
-        for j in range(max_money + 1):
-            dp[i].append(0)
-
-    for i in range(1, n + 1):
-        for current_money in range(1, max_money + 1):
+    for i in range(1, len(names) + 1):
+        for current_money in range(1, MONEY + 1):
             if values[i-1] <= current_money:
-                dp[i][current_money] = max(dp[i-1][current_money], gains[i-1] + dp[i-1][current_money - values[i-1]])
+                combinaisons[i][current_money] = max(combinaisons[i-1][current_money], gains[i-1] + combinaisons[i-1][current_money - values[i-1]])
             else:
-                dp[i][current_money] = dp[i-1][current_money]
+                combinaisons[i][current_money] = combinaisons[i-1][current_money]
 
-    print(dp[n][max_money])
+    n = len(names)
+    chosen_actions = []
+    chosen_gains = []
+    chosen_values = []
+    gains_total = 0
+    max_money = MONEY
+
+    while max_money > 0 and n > 0:
+        if combinaisons[n][max_money] != combinaisons[n-1][max_money]:
+            gains_total += values[n-1] * (1 + gains[n-1])
+            chosen_gains.append(gains[n-1])
+            chosen_actions.append(names[n-1])
+            chosen_values.append(values[n - 1])
+            max_money -= values[n-1]
+        n -= 1
+
+    return chosen_values, gains_total, chosen_actions
 
 
-if __name__ == "__main__":
+def run_optimized():
 
+    start_time = time.time()
     datas = pd.read_excel(FILE_PATH, header=None)
     actions_names = datas[0].tolist()
     actions_values = datas[1].tolist()
     actions_gains = datas[2].tolist()
 
-    run_optimized(actions_gains, actions_values, MONEY)
+    chosen_values, gains_total, chosen_actions = pick_actions(actions_gains, actions_values, actions_names)
+
+    print(f"Start invest : {sum(chosen_values)}")
+    print(f"Total value after 2y : {round(gains_total, 2)}")
+    print(f"Recommended actions : {chosen_actions}")
+
+    total_time = time.time() - start_time
+    print(round(total_time, 2))
+
+
+if __name__ == "__main__":
+    run_optimized()
